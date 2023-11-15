e6e745fdf6cf8423d2c33d40d60530afde1cf1bd
ayecue <soerenwehmeier@googlemail.com>
2023-11-15 17:15:18 +0100

    0.6.1 bump interpreter + override toString

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import './overrides';
+
+export { CLASS_ID_PROPERTY } from './overrides';
 export { CPS, defaultCPSVisit } from './cps';
 export { Interpreter } from './interpreter';
 export { Import } from './operations/import';
---
+++ src/overrides.ts
@@ -0,0 +1,11 @@
+import { CustomMap, CustomString } from "greybel-interpreter";
+
+export const CLASS_ID_PROPERTY = new CustomString('classID');
+const defaultCustomMapToString = CustomMap.prototype.toString;
+
+CustomMap.prototype.toString = function(depth: number = 0) {
+  if (this.value.has(CLASS_ID_PROPERTY)) {
+    return this.value.get(CLASS_ID_PROPERTY).toString();
+  }
+  return defaultCustomMapToString.call(this, depth);
+};
